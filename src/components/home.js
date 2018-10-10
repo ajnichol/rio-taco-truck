d54016ed7b79d89fcb2acf8a9cde8904bab23dcd
CORTANA\alex <anichols28@hotmail.com>
2018-10-10 15:54:46 -0700

    google maps is loading, passing in longitude and latitude data

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import jquery from 'jquery';
+import $ from 'jquery';
 
 class Home extends React.Component {
 
@@ -13,10 +13,10 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
+    $("#map").hide();
     axios.get(
       "https://my.api.mockaroo.com/locations.json?key=a45f1200"
     ).then(function(response) {
-      console.log(response.data);
       this.setState({
         locationData: response.data
       })
@@ -25,32 +25,47 @@ class Home extends React.Component {
     });
   }
 
+  handleClick(lat, long) {
+    $("#initialScreen").hide();
+    $("#map").show();
+    console.log(lat, long);
+    console.log("https://maps.googleapis.com/maps/api/staticmap?center=" + lat + ',' + long + "&zoom=13&size=700x1100&maptype=roadmap&key=AIzaSyDVmjqsuARE1SElhTzhEf7FEVDWITKw3iA");
+    let mapsLink = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + ',' + long + "&zoom=13&size=700x1100&maptype=roadmap&key=AIzaSyDVmjqsuARE1SElhTzhEf7FEVDWITKw3iA";
+    sessionStorage.setItem("Map", mapsLink);
+    console.log(sessionStorage);
+  }
+
   render() {
 
     let locationCard = this.state.locationData.map(function(truckInfo, index) {
       return (
-        <div key={truckInfo.id}>
+        <div key={truckInfo.id} onClick={() => this.handleClick(truckInfo.latitude, truckInfo.longitude)}>
           <div className="card" style={{width: 18 + 'rem'}}>
             <div className="card-body">
               <h5 className="card-title">Taco Truck {truckInfo.id}</h5>
               <p className="card-text">{truckInfo.address} <br /> {truckInfo.city}, {truckInfo.state} {truckInfo.postal_code}</p>
               <p className="card-text" id="closingTime">Open today until {truckInfo.saturday_close}</p>
-              <a href="#" className="card-link">Card link</a>
-              <a href="#" className="card-link">Another link</a>
+              <p className="card-text" id="truckPhone"><i className="fa fa-phone-square" aria-hidden="true"></i> 1-(800)-Taco-Truck</p>
+              <a href="#" className="card-link">Directions</a>
+              <a href="#" className="card-link">More Info</a>
             </div>
           </div>
         </div>
       )
-    });
+    }.bind(this));
 
     return (
       <div>
         <div className="container">
           <div className="row">
-            <div className="col-xs-12 col-md-6">
+            <div className="col-xs-12 col-md-5" id="cardContainer">
               {locationCard}
             </div>
-            <div className="col-xs-12 col-md-6">
+            <div className="col-xs-12 col-md-7">
+              <div className="text-center" id="initialScreen">
+                <p>Click a location card to load a map</p>
+              </div>
+              <img src={sessionStorage.getItem("Map")} />
             </div>
           </div>
         </div>

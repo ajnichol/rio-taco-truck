a62ade11db5b2bf4f262818a4539ff22b610f5ad
CORTANA\alex <anichols28@hotmail.com>
2018-10-10 00:34:02 -0700

    added desktop and mobile navs, get request successful and generating individual info cards

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import axios from 'axios';
+import jquery from 'jquery';
 
 class Home extends React.Component {
-  render () {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      locationData: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(
+      "https://my.api.mockaroo.com/locations.json?key=a45f1200"
+    ).then(function(response) {
+      console.log(response.data);
+      this.setState({
+        locationData: response.data
+      })
+    }.bind(this)).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+
+    let locationCard = this.state.locationData.map(function(truckInfo, index) {
+      return (
+        <div key={truckInfo.id}>
+          <div className="card" style={{width: 18 + 'rem'}}>
+            <div className="card-body">
+              <h5 className="card-title">Taco Truck {truckInfo.id}</h5>
+              <p className="card-text">{truckInfo.address} <br /> {truckInfo.city}, {truckInfo.state} {truckInfo.postal_code}</p>
+              <p className="card-text" id="closingTime">Open today until {truckInfo.saturday_close}</p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
+        </div>
+      )
+    });
+
     return (
-      <h1>Home</h1>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12 col-md-6">
+              {locationCard}
+            </div>
+            <div className="col-xs-12 col-md-6">
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Nav extends React.Component {
+
+  componentDidMount() {
+    if($(window).width() < 550) {
+      $("#desktopNav").hide();
+      $("#mobileNav").show();
+    }
+    else {
+      $("#desktopNav").show();
+      $("#mobileNav").hide();
+    }
+  }
+
   render () {
     return (
-      <h1>Nav</h1>
+      <div>
+        <nav className="navbar navbar-expand-lg" id="desktopNav">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              <img id="rioSEOLogo" src={require("../assets/rio-seo-logo.png")} alt="Rio SEO Logo" />
+            </a>
+
+            <div>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Menu</a>
+                </li>
+                <li className="nav-item">
+                  <span className="navbar-text">
+                    <i className="fa fa-map-marker" aria-hidden="true"></i> Truck Locator
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div id="mobileNav">
+          <div className="container text-center">
+            <div className="row">
+              <div className="col-4">
+                <a className="navbar-brand" href="#">
+                  <img id="mobileMenu" src={require("../assets/menu.png")} alt="Collapsed Menu Button" />
+                </a>
+              </div>
+              <div className="col-4">
+                <a href="#">
+                  <img id="mobileLogo" src={require("../assets/rioSEOMobileLogo.png")} alt="Rio SEO Mobile Logo" />
+                </a>
+              </div>
+              <div className="col-4">
+                <span className="navbar-text">
+                  <i className="fa fa-map-marker fa-2x" aria-hidden="true"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }

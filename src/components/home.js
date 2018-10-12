ff38ae7d1e8ec3c2357be116b542e9e0bf33d8c8
CORTANA\alex <anichols28@hotmail.com>
2018-10-11 18:08:48 -0700

    clicking more info button now displays a list of store hours along with other info

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,8 @@ class Home extends React.Component {
 
     this.state = {
       locationData: [],
-      googleMapURL: "https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=11&size=700x1100&maptype=roadmap&key=AIzaSyDVmjqsuARE1SElhTzhEf7FEVDWITKw3iA"
+      googleMapURL: "https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=11&size=700x1100&maptype=roadmap&key=AIzaSyDVmjqsuARE1SElhTzhEf7FEVDWITKw3iA",
+      modalInfo: {}
     }
   }
 
@@ -26,28 +27,33 @@ class Home extends React.Component {
     });
   }
 
-  handleClick(lat, long) {
+  handleCard(lat, long, truckInfo) {
+    console.log(truckInfo);
     $("#initialScreen").hide();
     $("#map").show();
-    console.log(lat, long);
     let mapsLink = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + ',' + long + "&zoom=14&size=700x1100&maptype=roadmap&markers=color:red%7C" + lat + ',' + long + "&key=AIzaSyDVmjqsuARE1SElhTzhEf7FEVDWITKw3iA";
     this.setState({
-      googleMapURL: mapsLink
+      googleMapURL: mapsLink,
+      modalInfo: truckInfo
     });
   }
 
-  render() {
+  handleDirections(address, city, state, zip) {
+    let link = "https://www.google.com/maps/place/" + address + "," + city + "," + state + " " + zip + "/";
+    window.open(link);
+  }
 
+  render() {
     let locationCard = this.state.locationData.map(function(truckInfo, index) {
       return (
-        <div key={truckInfo.id} onClick={() => this.handleClick(truckInfo.latitude, truckInfo.longitude)}>
-          <div className="card" style={{width: 18 + 'rem'}}>
+        <div key={truckInfo.id} onClick={() => this.handleCard(truckInfo.latitude, truckInfo.longitude, truckInfo)}>
+          <div className="card">
             <div className="card-body">
               <h5 className="card-title">Taco Truck {truckInfo.id}</h5>
               <p className="card-text">{truckInfo.address} <br /> {truckInfo.city}, {truckInfo.state} {truckInfo.postal_code}</p>
               <p className="card-text" id="closingTime">Open today until {truckInfo.saturday_close}</p>
               <p className="card-text" id="truckPhone"><i className="fa fa-phone-square" aria-hidden="true"></i> 1-(800)-Taco-Truck</p>
-              <a href="#" className="card-link">Directions</a>
+              <a href="#" className="card-link" onClick={() => this.handleDirections(truckInfo.address, truckInfo.city, truckInfo.state, truckInfo.postal_code)}>Directions</a>
               <a href="#" className="card-link">More Info</a>
             </div>
           </div>
@@ -66,7 +72,46 @@ class Home extends React.Component {
               <div className="text-center" id="initialScreen">
                 <p>Click a location card to load a map</p>
               </div>
-              <img src={this.state.googleMapURL} />
+              <img id="map" src={this.state.googleMapURL} />
+              <div className="row">
+                <div className="col-12 pull-right">
+                  <p>X</p>
+                </div>
+                <div className="col-12 text-center">
+                  <img id="placeholderImage" src={require("../assets/placeholder.png")} />
+                </div>
+                <div className="col-12">
+                  <h5 className="card-title">Taco Truck {this.state.modalInfo.id}</h5>
+                  <p className="card-text">{this.state.modalInfo.address} <br /> {this.state.modalInfo.city}, {this.state.modalInfo.state} {this.state.modalInfo.postal_code}</p>
+                </div>
+                <div className="col-6">
+                  <p className="card-text" id="truckPhone"><i className="fa fa-phone-square" aria-hidden="true"></i> 1-(800)-Taco-Truck</p>
+                </div>
+                <div className="col-6">
+                  <p className="card-text" id="truckPhone"><i className="fa fa-car" aria-hidden="true"></i> Get Directions</p>
+                </div>
+                <div className="col-6">
+                  <p>Monday</p>
+                  <p>Tuesday</p>
+                  <p>Wednesday</p>
+                  <p>Thursday</p>
+                  <p>Friday</p>
+                  <p>Saturday</p>
+                  <p>Sunday</p>
+                </div>
+                <div className="col-6">
+                  <p>{this.state.modalInfo.monday_open} - {this.state.modalInfo.monday_close}</p>
+                  <p>{this.state.modalInfo.tuesday_open} - {this.state.modalInfo.tuesday_close}</p>
+                  <p>{this.state.modalInfo.wednesday_open} - {this.state.modalInfo.wednesday_close}</p>
+                  <p>{this.state.modalInfo.thursday_open} - {this.state.modalInfo.thursday_close}</p>
+                  <p>{this.state.modalInfo.friday_open} - {this.state.modalInfo.friday_close}</p>
+                  <p>{this.state.modalInfo.saturday_open} - {this.state.modalInfo.saturday_close}</p>
+                  <p>{this.state.modalInfo.sunday_open} - {this.state.modalInfo.sunday_close}</p>
+                </div>
+                <div className="col-12 text-center">
+                  <a href={this.state.modalInfo.url} target="_blank" id="fullDetailsBtn">View Full Details</a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
